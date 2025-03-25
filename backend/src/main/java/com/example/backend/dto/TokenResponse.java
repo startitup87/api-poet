a2edcd28f4ee5cc9c0b393040c4fa92c6d868f13
startitup87 <dbmoreland87@gmail.com>
2025-03-24 23:34:54 -0400

    Update TokenResponse.java

--- backend/src/main/java/com/example/backend/dto/TokenResponse.java
+++ backend/src/main/java/com/example/backend/dto/TokenResponse.java
@@ -2,13 +2,11 @@ package com.example.backend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class TokenResponse {
     private String accessToken;
     private String refreshToken;
-    private String tokenType = "Bearer";
+    private String tokenType;
 }
