a467773372b881b30dea16b81580704a99eb960c
startitup87 <dbmoreland87@gmail.com>
2025-03-24 23:33:28 -0400

    Update LoginRequest.java

--- backend/src/main/java/com/example/backend/dto/LoginRequest.java
+++ backend/src/main/java/com/example/backend/dto/LoginRequest.java
@@ -7,7 +7,7 @@ import lombok.Data;
 public class LoginRequest {
     @NotBlank(message = "Username is required")
     private String username;
-    
+
     @NotBlank(message = "Password is required")
     private String password;
 }
