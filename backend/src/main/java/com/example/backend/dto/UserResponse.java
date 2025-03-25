1f714cf02c172ddbfb4fe4dfcc23f79d4c630a38
startitup87 <dbmoreland87@gmail.com>
2025-03-24 23:35:40 -0400

    Update UserResponse.java

--- backend/src/main/java/com/example/backend/dto/UserResponse.java
+++ backend/src/main/java/com/example/backend/dto/UserResponse.java
@@ -1,12 +1,10 @@
 package com.example.backend.dto;
 
 import lombok.Data;
-import java.util.List;
 
 @Data
 public class UserResponse {
     private Long id;
     private String username;
     private String email;
-    private List<String> roles;
 }
