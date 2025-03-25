dc720e3c3484fa1c324bf01a280464974d56ab70
startitup87 <dbmoreland87@gmail.com>
2025-03-24 23:34:23 -0400

    Update RegisterRequest.java

--- backend/src/main/java/com/example/backend/dto/RegisterRequest.java
+++ backend/src/main/java/com/example/backend/dto/RegisterRequest.java
@@ -2,20 +2,17 @@ package com.example.backend.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
     @NotBlank(message = "Username is required")
-    @Size(min = 4, message = "Username must be at least 4 characters")
     private String username;
-    
+
+    @Email(message = "Invalid email")
     @NotBlank(message = "Email is required")
-    @Email(message = "Email must be valid")
     private String email;
-    
+
     @NotBlank(message = "Password is required")
-    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
 }
